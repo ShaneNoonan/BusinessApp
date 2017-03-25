77e36451ff821db333b13c19d547f8e9895549e2
ShaneNoonan <ShaneNoonan@MacBook-Pro.local>
2017-03-25 23:26:10 +0000

    Initial Commit

---
+++ BusinessApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BusinessApp
+//
+//  Created by Shane Noonan on 25/03/2017.
+//  Copyright © 2017 Shane Noonan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
